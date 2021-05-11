d6e783e2ca1627a8222a6de7a538c11249a61582
John Schellinger <schellingerj1@gmail.com>
2021-05-10 20:30:17 -0400

    Fix query params for user by id route

--- routes/user-route.js
+++ routes/user-route.js
@@ -6,7 +6,7 @@ const { getItem, scan } = require('../services/database-service');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  const userId = req.query.id;
+  const userId = req.query.userId;
   getItem(userId)
     .then((user) => res.json(user))
     .catch((error) => console.error(error));
